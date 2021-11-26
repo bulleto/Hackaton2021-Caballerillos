658be172a57e07d22259c1af42d2f82112276343
Adriel Morales <74667660+ddevil9910@users.noreply.github.com>
2021-11-26 11:53:56 -0600

    db

--- partials/conexion.php
+++ partials/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$database = "caballerillos";
-$username = "root";
-$password = "";
+$database = "id17593454_caballerillos";
+$username = "id17593454_mike";
+$password = "M#ihg[^h8X^2%AC";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
