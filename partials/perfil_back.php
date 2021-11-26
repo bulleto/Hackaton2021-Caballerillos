ae7a9f726c195f3797b9893ccb8fceef98069db3
bulleto <63422696+bulleto@users.noreply.github.com>
2021-11-26 07:37:09 -0600

    perfil

---
+++ partials/perfil_back.php
@@ -0,0 +1,33 @@
+<?php
+
+
+if(isset($_GET['id'])) {
+    include_once 'conexion.php';
+    $encoded_id = $_GET['id'];
+    $decode_id = base64_decode($encoded_id);
+    $user_id_array = explode("encodeuserid", $decode_id);
+    $id = $user_id_array[1];
+
+    try {
+
+        //se realiza la consulta
+        $stmt = $conn->prepare("SELECT usuario,nombre,apellidos,foto,urgencias,historial FROM usuarios WHERE id='$id'");
+        $stmt->execute();
+
+        //loguear el usuario
+
+        $stmt->bind_result($db_usuario,$db_nombre,$db_apellidos,$db_foto,$db_urgencias,$db_historial);
+        $stmt->fetch();
+
+
+
+
+    } catch (Exception $e) {
+        //se produce un error;
+        $arreglo = array(
+            'error' => $e->getMessage()
+        );
+    }
+}
+
+?>
